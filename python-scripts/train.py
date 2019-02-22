1b420d370ea0cf7b401bd88768bf9cc5d4b5766c
antonio <antonior92@gmail.com>
2019-02-22 12:57:35 +0100

    Improvements on training loop

--- python-scripts/train.py
+++ python-scripts/train.py
@@ -5,20 +5,23 @@ import torch.nn as nn
 import torch.optim as optim
 from tcn import TCN
 from data_generation import chen_example
-from model_eval import get_input
+from model_eval import get_input, one_step_ahead
 
-args = {'ksize': 7,
+args = {'ksize': 3,
         'lr': 0.001,
         'cuda': False,
         'dropout': 0.8,
         'seed': 1111,
         'optim': 'Adam',
         'batch_size': 3,
-        'log_interval': 10,
+        'log_interval': 1,
         'ar': True,
         'epochs': 1000,
         'plot': True,
-        'plotly': True}
+        'plotly': True,
+        'lr_scheduler_nepochs': 10,
+        'lr_scheduler_factor': 10,
+        'min_lr': 1e-6}
 
 torch.manual_seed(args['seed'])
 if torch.cuda.is_available():
@@ -45,13 +48,15 @@ if args['cuda']:
     y_val = y_val.cuda()
 
 x_train = get_input(u_train, y_train, args['ar'])
+x_val = get_input(u_val, y_val, args['ar'])
 nx = x_train.size()[1]
 
 # Neural network
-n_channels = [16, 32, 64, 128]
+n_channels = [16, 32]
+dilation_sizes = [1, 1, 1]
 kernel_size = args['ksize']
 dropout = args['dropout']
-model = TCN(nx, ny, n_channels, kernel_size=kernel_size, dropout=dropout)
+model = TCN(nx, ny, n_channels, kernel_size=kernel_size, dilation_sizes=dilation_sizes, dropout=dropout)
 if args['cuda']:
     model.cuda()
 
@@ -64,6 +69,24 @@ optimizer = getattr(optim, args['optim'])(model.parameters(), lr=lr)
 
 
 # %% Train
+
+def validate():
+    model.eval()
+    total_vloss = 0
+    for i in range(0, x_val.size()[0], batch_size):
+        if i + batch_size > x_train.size()[0]:
+            x, y = x_val[i:], y_val[i:]
+        else:
+            x, y = x_val[i:(i + batch_size)], y_val[i:(i + batch_size)]
+
+        output = model(x)
+        vloss = nn.MSELoss()(output, y)
+        processed = min(i + batch_size, x_train.size()[0])
+        total_vloss = i / processed * total_vloss + x.size()[0] / processed * vloss.item()
+
+    return total_vloss
+
+
 def train(epoch):
     model.train()
     total_loss = 0
@@ -83,18 +106,37 @@ def train(epoch):
         total_loss = i / processed * total_loss + x.size()[0] / processed * loss.item()
 
         if batch_idx % args['log_interval'] == 0:
-            print('Train Epoch: {:2d} [{:6d}/{:6d} ({:.0f}%)]\tLearning rate: {:.4f}\tLoss: {:.6f}'.format(
+            print('Train Epoch: {:5d} [{:6d}/{:6d} ({:3.0f}%)]\tLearning rate: {:.6f}\tLoss: {:.6f}'.format(
                 epoch, processed, x_train.size()[0], 100. * processed / x_train.size()[0], lr, total_loss))
-            batch_idx = 0
+        batch_idx += 1
 
     return total_loss
 
 
 all_losses = []
+all_vlosses = []
 best_vloss = 1e10
 for epoch in range(1, epochs+1):
+    # Train and validate
     loss = train(epoch)
+    vloss = validate()
+    # Save losses
     all_losses += [loss]
-    if loss < best_vloss:  # Use validation loss here instead of training loss
+    all_vlosses += [vloss]
+    if vloss < best_vloss:
         torch.save(model, 'best_model.pt')
-        best_vloss = loss
+        best_vloss = vloss
+    # Print validation results
+    print('-'*100)
+    print('Train Epoch: {:5d} [{:6d}/{:6d} ({:.0f}%)]\tLearning rate: {:.6f}\tLoss: {:.6f}\tVal Loss: {:.6f}'.format(
+            epoch, x_train.size()[0], x_train.size()[0], 100., lr, loss, vloss))
+    print('-'*100)
+    # lr scheduler
+    if epoch > args['lr_scheduler_nepochs'] and vloss > max(all_vlosses[-args['lr_scheduler_nepochs']-1:-1]):
+        print('bfaf')
+        lr = lr / args['lr_scheduler_factor']
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+    # Early stoping
+    if lr < args['min_lr']:
+        break
