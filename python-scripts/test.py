af34ee490eca2f2131cd75c7bbc326ddf9fa6005
antonio <antonior92@gmail.com>
2019-02-25 15:03:24 +0100

    Update testfile according to DynamicModel class

--- python-scripts/test.py
+++ python-scripts/test.py
@@ -1,22 +1,21 @@
 # %% Test
 import torch
-from model_eval import one_step_ahead
-from data_generation import chen_example
+from data_generation import DataLoaderExt, ChenDataset
 import matplotlib.pyplot as plt
 
-args = {'plotly': True,
-        'ar': True}
+args = {'plotly': True}
 
 model = torch.load('best_model.pt')
-u_test, y_test = chen_example(1000, 2)
-u_test, y_test = torch.tensor(u_test, dtype=torch.float), torch.tensor(y_test, dtype=torch.float)
+loader_test = DataLoaderExt(ChenDataset(seq_len=1000, ntotbatch=2, seed=3), batch_size=1,
+                            shuffle=False)
+model.set_mode('free-run-simulation')
 
 # Test
-for i in range(0, u_test.size()[0]):
+for i, (u, y) in enumerate(loader_test):
+    y_pred = model(u, y)
     fig, ax = plt.subplots()
-    y_pred = one_step_ahead(model, u_test, y_test, args['ar'])
-    ax.plot(y_test[i, 0, :].detach().numpy(), color='b', label='y true')
-    ax.plot(y_pred[i, 0, :].detach().numpy(), color='g', label='y_pred')
+    ax.plot(y[0, 0, :].detach().numpy(), color='b', label='y true')
+    ax.plot(y_pred[0, 0, :].detach().numpy(), color='g', label='y_pred')
 
     if args['plotly']:
         import plotly.tools as tls
