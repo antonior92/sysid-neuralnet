Cleaned up normalization, fixed small bug and setup new experiment

python-scripts/model/dynamic_model.py
@@ -6,11 +6,27 @@ from . import TCN
 from model.utils import RunMode
 
 
-class DynamicModel(nn.Module):
-    epsilon = 1e-16
+class Normalizer1D(nn.Module):
+    _epsilon = 1e-16
 
-    def __init__(self, model, num_inputs, num_outputs, ar, io_delay,
-                 offset_in=None, offset_out=None, scale_in=None, scale_out=None,
+    def __init__(self, scale, offset):
+        super(Normalizer1D, self).__init__()
+        self.scale = torch.tensor(scale, dtype=torch.float32) + self._epsilon
+        self.offset = torch.tensor(offset, dtype=torch.float32)
+
+    def normalize(self, x):
+        x = x.permute(0, 2, 1)
+        x = (x-self.offset)/self.scale
+        return x.permute(0, 2, 1)
+
+    def unnormalize(self, x):
+        x = x.permute(0, 2, 1)
+        x = x*self.scale + self.offset
+        return x.permute(0, 2, 1)
+
+
+class DynamicModel(nn.Module):
+    def __init__(self, model, num_inputs, num_outputs, ar, io_delay, normalizer_input=None, normalizer_output=None,
                  *args, **kwargs):
         super(DynamicModel, self).__init__()
         # Save parameters
@@ -21,14 +37,9 @@ class DynamicModel(nn.Module):
         self.ar = ar
         self.io_delay = io_delay
         self.is_cuda = False
-        self.offset_in = torch.tensor(offset_in, dtype=torch.float32) + self.epsilon if offset_in is not None \
-            else torch.zeros(num_inputs)
-        self.offset_out = torch.tensor(offset_out, dtype=torch.float32) + self.epsilon if offset_out is not None \
-            else torch.zeros(num_outputs)
-        self.scale_in = torch.tensor(scale_in, dtype=torch.float32) + self.epsilon if scale_in is not None \
-            else torch.ones(num_inputs)
-        self.scale_out = torch.tensor(scale_out, dtype=torch.float32) + self.epsilon if scale_out is not None \
-            else torch.ones(num_outputs)
+        self.normalizer_input = normalizer_input
+        self.normalizer_output = normalizer_output
+
         # Initialize model
         self.mode = RunMode.ONE_STEP_AHEAD
         if model == 'mlp':
@@ -56,23 +67,19 @@ class DynamicModel(nn.Module):
         self.mode = mode
         self.m.set_mode(mode)
 
-    def one_step_ahead(self, u, y=None):
-        u_normalized = self.tensor_mult(u, 1/self.scale_in, -self.offset_in/self.scale_in)
-        u_delayed = DynamicModel._get_u_delayed(u_normalized, self.io_delay)
+    def one_step_ahead(self, u, y):
+
+        u_delayed = DynamicModel._get_u_delayed(u, self.io_delay)
         if self.ar:
-            # Normalize y
-            y_normalized = self.tensor_mult(y, 1 / self.scale_out, -self.offset_out / self.scale_out)
-            y_delayed = F.pad(y_normalized[:, :, :-1], [1, 0])
+            y_delayed = F.pad(y[:, :, :-1], [1, 0])
             x = torch.cat((u_delayed, y_delayed), 1)
         else:
             x = u_delayed
         y_pred = self.m(x)
-        # Un-normalize y
-        y_pred = self.tensor_mult(y_pred, self.scale_out, self.offset_out)
 
         return y_pred
 
-    def free_run_simulation(self, u, y=None):
+    def free_run_simulation(self, u, y):
         if self.ar:
             rf = self.m.receptive_field
             num_batches, _, seq_len = u.size()
@@ -80,9 +87,8 @@ class DynamicModel(nn.Module):
             if self.is_cuda:
                 y_sim = y_sim.cuda()
 
-            u_normalized = self.tensor_mult(u, 1 / self.scale_in, -self.offset_in / self.scale_in)
-            u_delayed = DynamicModel._get_u_delayed(u_normalized, self.io_delay)
-            # Nomalize input
+            u_delayed = DynamicModel._get_u_delayed(u, self.io_delay)
+
             for i in range(seq_len):
                 if i < rf:
                     y_in = y_sim[:, :, :i+1]
@@ -94,8 +100,6 @@ class DynamicModel(nn.Module):
                 x = torch.cat((u_in, y_in), 1)
 
                 y_sim[:, :, i+1] = self.m(x)[:, :, -1]
-            # Un-normalize output
-            y_sim = self.tensor_mult(y_sim, self.scale_out, self.offset_out)
         else:
             y_sim = self.one_step_ahead(u, y)
         return y_sim[..., 1:]
@@ -108,20 +112,21 @@ class DynamicModel(nn.Module):
             u_delayed = F.pad(u[:, :, io_delay:], [0, io_delay])
         else:
             u_delayed = u
-
         return u_delayed
 
-    @staticmethod
-    def tensor_mult(x, scale, offset):
-        x = x.permute(0, 2, 1)
-        x = x.mul(scale) + offset
-        return x.permute(0, 2, 1)
+    def forward(self, u, y=None):
+        if self.normalizer_input is not None:
+            u = self.normalizer_input.normalize(u)
+        if y is not None and self.normalizer_output is not None:
+            y = self.normalizer_output.normalize(y)
 
-    def forward(self, *args):
         if self.mode == RunMode.ONE_STEP_AHEAD:
-            return self.one_step_ahead(*args)
+            y_pred = self.one_step_ahead(u, y)
         elif self.mode == RunMode.FREE_RUN_SIMULATION:
-            return self.free_run_simulation(*args)
+            y_pred = self.free_run_simulation(u, y)
         else:
             raise Exception("Not implemented mode")
 
+        if self.normalizer_output is not None:
+            y_pred = self.normalizer_output.unnormalize(y_pred)
+        return y_pred

python-scripts/model/model_state.py
@@ -13,20 +13,13 @@ class ModelState:
     optimizer
     """
 
-    def __init__(self, seed, cuda, nu, ny, optimizer, init_lr, model, model_options,
-                 normalize, normalize_n_std, u_mean, y_mean, u_std, y_std):
+    def __init__(self, seed, cuda, nu, ny, optimizer, init_lr, model, model_options, **kwargs):
         torch.manual_seed(seed)
         if torch.cuda.is_available():
             if not cuda:
                 print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-        if normalize:
-            self.model = DynamicModel(model, nu, ny,
-                                      offset_in=u_mean, scale_in=normalize_n_std*u_std,
-                                      offset_out=y_mean, scale_out=normalize_n_std*y_std,
-                                      **model_options)
-        else:
-            self.model = DynamicModel(model, nu, ny, **model_options)
+        self.model = DynamicModel(model, nu, ny, **model_options, **kwargs)
 
         if cuda:
             self.model.cuda()
@@ -35,21 +28,26 @@ class ModelState:
         self.optimizer = getattr(optim, optimizer['optim'])(self.model.parameters(), lr=init_lr)
 
     def load_model(self, path, name='model.pt'):
-        try:
-            file = os.path.join(path, name)
-            ckpt = torch.load(file, map_location=lambda storage, loc: storage)
-            self.model.load_state_dict(ckpt["model"])
-            self.optimizer.load_state_dict(ckpt["optimizer"])
-            epoch = ckpt['epoch']
-        except NotADirectoryError as e:
+        if os.path.isfile(path):
             try:
                 file = path
                 ckpt = torch.load(file, map_location=lambda storage, loc: storage)
                 self.model.load_state_dict(ckpt["model"])
                 self.optimizer.load_state_dict(ckpt["optimizer"])
                 epoch = ckpt['epoch']
-            except NotADirectoryError as e:
-                raise Exception("Could not find model: " + path)
+            except NotADirectoryError:
+                raise Exception("Could not find model: " + file)
+
+        else:
+            try:
+                file = os.path.join(path, name)
+                ckpt = torch.load(file, map_location=lambda storage, loc: storage)
+                self.model.load_state_dict(ckpt["model"])
+                self.optimizer.load_state_dict(ckpt["optimizer"])
+                epoch = ckpt['epoch']
+            except NotADirectoryError:
+                raise Exception("Could not find model: " + file)
+
         return epoch
 
     def save_model(self, epoch, vloss, elapsed_time,  path, name='model.pt'):

python-scripts/model/tcn.py
@@ -29,7 +29,7 @@ class TemporalBlock(DynamicModule):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, dropout=0.2):
         super(TemporalBlock, self).__init__()
 
-        # This is the receptive field of the entire module!
+        # The padding of one conv and the receptive field of the entire module
         self.padding = (kernel_size - 1) * dilation
         self.receptive_field = 2 * self.padding + 1
         self.requested_output = None
@@ -50,13 +50,6 @@ class TemporalBlock(DynamicModule):
                                  self.pad2, self.conv2, self.relu2, self.dropout2)
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
         self.relu = nn.ReLU()
-        self.init_weights()
-
-    def init_weights(self):
-        self.conv1.weight.data.normal_(0, 0.01)
-        self.conv2.weight.data.normal_(0, 0.01)
-        if self.downsample is not None:
-            self.downsample.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
         res = x if self.downsample is None else self.downsample(x)
@@ -103,17 +96,12 @@ class TCN(DynamicModule):
             layers += [TemporalBlock(in_channels, out_channels, ksize, stride=1, dilation=dilation_size, dropout=dropout)]
         self.network = nn.Sequential(*layers)
         self.final_conv = nn.Conv1d(n_channels[-1], num_outputs, 1)
-        self.init_weights()
 
         requested_output = 1
         for temporal_module in reversed(self.network):
             temporal_module.requested_output = requested_output
             requested_output += temporal_module.receptive_field - 1
         self.receptive_field = requested_output
-
-
-    def init_weights(self):
-        self.final_conv.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
         y = self.network(x)

python-scripts/run.py
@@ -7,8 +7,9 @@ import train
 from logger import set_redirects
 import data.loader as loader
 from model.model_state import ModelState
+from model.dynamic_model import Normalizer1D
 import torch
-from model.utils import RunMode
+import numpy as np
 
 default_options_lstm = {
     'hidden_size': 5,
@@ -48,12 +49,12 @@ default_options_chen = {
     }
 }
 
-default_options_silverbox = {'seq_len': 1000, 'seq_len_eval': 1000}
+default_options_silverbox = {'seq_len': 2048, 'seq_len_eval': 2048}
 
 default_options_train = {
         'init_lr': 0.001,
         'min_lr': 1e-6,
-        'batch_size': 3,
+        'batch_size': 6,
         'epochs': 10000,
         'lr_scheduler_nepochs': 10,
         'lr_scheduler_factor': 10,
@@ -78,7 +79,7 @@ default_options = {
     'run_name': None,
     'load_model': None,
     'evaluate_model': False,
-    'normalize': False,
+    'normalize': True,
     'normalize_n_std': 1,
     'train_options': default_options_train,
     'test_options': default_options_test,
@@ -232,26 +233,27 @@ def create_full_options_dict(*option_dicts):
     return options
 
 
-def compute_mean(loader_train, cuda):
+def compute_normalizers(loader_train, variance_scaler):
     total_batches = 0
+    u_mean = 0
+    y_mean = 0
+    u_var = 0
+    y_var = 0
     for i, (u, y) in enumerate(loader_train):
         total_batches += u.size()[0]
-        if cuda:
-            u = u.cuda()
-            y = y.cuda()
-        if i == 0:
-            u_mean = torch.mean(u, dim=(0, 2))
-            y_mean = torch.mean(y, dim=(0, 2))
-            u_var = torch.mean(torch.var(u, dim=2, unbiased=False), dim=0)
-            y_var = torch.mean(torch.var(y, dim=2, unbiased=False), dim=0)
-        else:
-            u_mean += torch.mean(u, dim=(0, 2))
-            y_mean += torch.mean(y, dim=(0, 2))
-            u_var += torch.mean(torch.var(u, dim=2, unbiased=False), dim=0)
-            y_var += torch.mean(torch.var(y, dim=2, unbiased=False), dim=0)
+        u_mean += torch.mean(u, dim=(0, 2))
+        y_mean += torch.mean(y, dim=(0, 2))
+        u_var += torch.mean(torch.var(u, dim=2, unbiased=False), dim=(0, ))
+        y_var += torch.mean(torch.var(y, dim=2, unbiased=False), dim=(0, ))
 
-    return (u_mean/total_batches, y_mean/total_batches,
-           torch.sqrt(u_var/total_batches), torch.sqrt(y_var/total_batches))
+    u_mean = u_mean.numpy()
+    y_mean = y_mean.numpy()
+    u_var = u_var.numpy()
+    y_var = y_var.numpy()
+
+    u_normalizer = Normalizer1D(np.sqrt(u_var/total_batches)*variance_scaler, u_mean/total_batches)
+    y_normalizer = Normalizer1D(np.sqrt(y_var/total_batches)*variance_scaler, y_mean/total_batches)
+    return u_normalizer, y_normalizer
 
 
 def run(options=None, load_model=None, mode_interactive=True):
@@ -276,6 +278,9 @@ def run(options=None, load_model=None, mode_interactive=True):
         options["model_options"] = ckpt_options["model_options"]
         options["dataset_options"] = ckpt_options["dataset_options"]
 
+        options["normalize"] = ckpt_options["normalize"]
+        options["normalize_n_std"] = ckpt_options["normalize_n_std"]
+
         options = recursive_merge(ckpt_options, options)
     elif options is None:
         options = create_full_options_dict()  # Default values
@@ -285,8 +290,12 @@ def run(options=None, load_model=None, mode_interactive=True):
                                   dataset_options=options["dataset_options"],
                                   train_batch_size=options["train_options"]["batch_size"],
                                   test_batch_size=options["test_options"]["batch_size"])
-    # Compute mean and var
-    u_mean, y_mean, u_std, y_std = compute_mean(loaders['train'], options["cuda"])
+
+    # Compute normalizers
+    if options["normalize"]:
+        normalizer_input, normalizer_output = compute_normalizers(loaders['train'], options["normalize_n_std"])
+    else:
+        normalizer_input = normalizer_output = None
 
     # Define model
     modelstate = ModelState(seed=options["seed"],
@@ -296,12 +305,8 @@ def run(options=None, load_model=None, mode_interactive=True):
                             init_lr=options["train_options"]["init_lr"],
                             model=options["model"],
                             model_options=options["model_options"],
-                            normalize=options["normalize"],
-                            normalize_n_std=options["normalize_n_std"],
-                            u_mean=u_mean,
-                            y_mean=y_mean,
-                            u_std=u_std,
-                            y_std=y_std)
+                            normalizer_input=normalizer_input,
+                            normalizer_output=normalizer_output)
 
     # Restore model
     if load_model is not None: