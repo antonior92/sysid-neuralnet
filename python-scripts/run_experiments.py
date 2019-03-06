a7adb5efaeb30e6b39996467147ec31d5ce244e6
Carl Andersson <calle@flaton.se>
2019-03-06 11:10:34 +0100

    New run

--- python-scripts/run_experiments.py
+++ python-scripts/run_experiments.py
@@ -19,11 +19,11 @@ lr_list = [0.001*math.sqrt(0.1)**i for i in range(4)]
 
 for max_past_input in mlp_max_past_input_list:
     for hidden_size in mlp_hidden_size_list:
-        option_dicts.append({"logdir": "log/mlp_with_normalization_3", "cuda": True,
+        option_dicts.append({"logdir": "log/mlp_with_normalization_4", "cuda": True,
                              "dataset": "silverbox", "model": "mlp",
                              "normalize": True, "normalize_n_std": 1,
                              "model_options": {"max_past_input": max_past_input, "hidden_size": hidden_size,
-                                               "activation_fn": "relu"}
+                                               "activation_fn": "elu"}
                              }
                             )
 
