05468c0b02d062f0f77f4bc57a7dae32ee1c1c86
antonio <antonior92@gmail.com>
2019-03-18 17:11:29 +0100

    Changes on validation results

--- python-scripts/validate/chen.py
+++ python-scripts/validate/chen.py
@@ -15,6 +15,8 @@ df = pd.concat([df1, df2], axis=0, ignore_index=True, sort=False)
 # Get only results from chen example
 results = df[df['dataset'] == 'chen']
 
+results['rmse'] = results.apply(lambda x: np.sqrt(x['vloss']), axis=1)
+
 # Create one dataframe for each model type
 results_mlp = results[results['model'] == 'mlp']
 results_tcn = results[results['model'] == 'tcn']
@@ -34,83 +36,92 @@ for i, m in enumerate(models):
             aux = results[(results['model'] == m) &
                           (results['train_sd_v'] == sd_v) &
                           (results['train_ntotbatch'] == n_batch)]
-            entry = aux.ix[aux["vloss"].idxmin()]
+            entry = aux.ix[aux["rmse"].idxmin()]
 
-            mins[i, j, k] = entry["vloss"]
+            mins[i, j, k] = entry["rmse"]
 
 mins_da = xr.DataArray(mins,
                        coords={'model': models,
                                'noise_levels': noise_levels,
                                'n_batches': n_batches},
                        dims=['model', 'noise_levels', 'n_batches'])
-mins_df = mins_da.to_dataframe('i').unstack().swaplevel(axis=0).sort_index(level=0)
+mins_df = mins_da.to_dataframe('i').unstack().swaplevel(axis=0).unstack().sort_index(level=0)
 
 print(mins_df)
+mins_df.to_csv('optimal_models_chen.csv', float_format='%.3f')
 
 
 # %% (TCN) Dropout effect (global)
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
+sns.boxplot(y='rmse',
              x='model_options_dropout',
-             hue='train_ntotbatch',
-             style='train_sd_v',
-             data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
+             data=results_tcn[(results_tcn['train_sd_v'] == 0.3) &
+                              (results_tcn['train_ntotbatch'] == 20)],
+            )
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('dropout rate', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+plt.tight_layout()
+plt.savefig('dropout_chen.png')
 show_fig(fig, False)
 
-# %% (TCN) Dropout effect (batch norm, weight norm, nothing)
+# %% (TCN) batch norm, weight norm, nothing
+
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             x='model_options_dropout',
-             hue='model_options_normalization',
-             style='train_sd_v',
-             data=results_tcn[(results['train_ntotbatch'] == 20)],
-             legend='full',
-             estimator='min',
-             ci=None)
+sns.boxplot(y='rmse',
+            x='model_options_normalization',
+            data=results_tcn[(results_tcn['train_sd_v'] == 0.3) &
+                              (results_tcn['train_ntotbatch'] == 20)],
+            )
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+plt.tight_layout()
+plt.savefig('normalization_chen.png')
+
 show_fig(fig, False)
+
 
 # %% (TCN) Dilations effects
 
-fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             x='model_options_n_channels_len',
-             hue='model_options_dilation_sizes_1',
-             style='train_sd_v',
-             data=results_tcn[(results['train_ntotbatch'] == 5)],
-             legend='full',
-             estimator='min',
-             ci=None)
-show_fig(fig, False)
-
-# %% (TCN) Dropout effect (n layers)
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             x='model_options_dropout',
-             hue='model_options_n_channels_len',
-             style='train_sd_v',
-             data=results_tcn[(results['train_ntotbatch'] == 20)],
-             legend='full',
-             estimator='min',
-             ci=None)
+sns.boxplot(y='rmse',
+            x='model_options_dilation_sizes_1',
+            data=results_tcn[(results_tcn['train_sd_v'] == 0.3) &
+                              (results_tcn['train_ntotbatch'] == 20)],
+            )
+
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+ax.set_xticklabels(['no dilations', 'with dilations'])
+plt.tight_layout()
+plt.savefig('dilations_chen.png')
 show_fig(fig, False)
+
 
 
 # %% (TCN) Effect of the number of n layers (global)
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             x='model_options_n_channels_len',
-             hue='train_ntotbatch',
-             style='train_sd_v',
-             data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
+sns.boxplot(y='rmse',
+            x='model_options_n_channels_len',
+            data=results_tcn[(results_tcn['train_sd_v'] == 0.3) &
+                              (results_tcn['train_ntotbatch'] == 20)],
+            )
+
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('number of residual blocks', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+ax.set_xticklabels([1, 2, 4, 8])
+plt.tight_layout()
+plt.savefig('nlayers_chen.png')
 show_fig(fig, False)
 
 # %% (TCN) Effect of the number of channels/layers
@@ -136,6 +147,7 @@ sns.lineplot(y='vloss',
              legend='full',
              estimator='min',
              ci=None)
+ax.set_xscale('log')
 show_fig(fig, False)
 
 # %% (TCN) Effect of the number of channels/layers/norm
@@ -185,8 +197,9 @@ sns.lineplot(y='vloss',
              x='model_options_hidden_size',
              hue='model_options_num_layers',
              style='train_sd_v',
-             data=results_lstm[(results['train_ntotbatch'] == 5)],
+             data=results_lstm[(results['train_ntotbatch'] == 20)],
              legend='full',
              estimator='min',
              ci=None)
-show_fig(fig, True)
+ax.set_xscale('log')
+show_fig(fig, False)
--- python-scripts/validate/f16gvt.py
+++ python-scripts/validate/f16gvt.py
@@ -7,14 +7,16 @@ import xarray as xr
 from utils import show_fig
 
 # %% Read file
-df1 = pd.read_csv('validate/val_code.csv')
+df1 = pd.read_csv('validate/val2_code.csv')
 df2 = pd.read_csv('validate/hydra8.csv')
-df3 = pd.read_csv('validate/val_hydra3.csv')
+df3 = pd.read_csv('validate/val2_hydra3.csv')
 
 df = pd.concat([df1, df2, df3], axis=0, ignore_index=True, sort=False)
 
 # Get only results from chen example
 results = df[df['dataset'] == 'f16gvt']
+
+results['rmse'] = results.apply(lambda x: np.sqrt(x['vloss']), axis=1)
 
 # Create one dataframe for each model type
 results_mlp = results[results['model'] == 'mlp']
@@ -30,78 +32,124 @@ print('len mlp = {}'.format(len(results_mlp)))
 models = ['mlp', 'tcn', 'lstm']
 
 mins = []
-
+logdir = []
 for i, m in enumerate(models):
     aux = results[(results['model'] == m)]
     mins += [aux.ix[aux["vloss"].idxmin()]["vloss"]]
+    logdir += [aux.ix[aux["vloss"].idxmin()]["logdir"]]
 
 
 df = pd.DataFrame(np.reshape(mins, (1, 3)), columns=models, index=[0])
 
 print(df)
 
-# %% (TCN) Dropout/nlayers effect
+# %% Get best models directories
+
+for i in range(3):
+    if sum(df1['logdir'] == logdir[i]):
+        print('Best {0} model is on code:~/Documents/sysid-neuralnet/python-scripts/{1}'.format(models[i], logdir[i]))
+
+for i in range(3):
+    if sum(df2['logdir'] == logdir[i]):
+        print('Best {0} model is on hydra8:~/Documents/sysid-neuralnet/python-scripts/{1}'.format(models[i], logdir[i]))
+
+for i in range(3):
+    if sum(df3['logdir'] == logdir[i]):
+        print('Best {0} model is on hydra3:~/Documents/sysid-neuralnet/python-scripts/{1}'.format(models[i], logdir[i]))
+
+# %% (TCN) Dropout effect
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             hue='model_options_dropout',
-             x='model_options_n_channels_len',
+sns.boxplot(y='rmse',
+             x='model_options_dropout',
              data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
-ax.set_yscale('log')
+            )
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('dropout rate', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+plt.tight_layout()
+plt.savefig('dropout_f16gvt.png')
 show_fig(fig, False)
 
-# %% (TCN) Batch norm/nlayers effect
+# %% (TCN) Batch norm
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             hue='model_options_normalization',
-             x='model_options_n_channels_len',
-             data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
-ax.set_yscale('log')
+sns.boxplot(y='rmse',
+            x='model_options_normalization',
+            data=results_tcn,
+            order=['weight_norm', 'batch_norm', 'none']
+
+            )
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+plt.tight_layout()
+plt.savefig('normalization_f16gvt.png')
+
 show_fig(fig, False)
 
 
-# %% (TCN) n_layers, num_channels effect
 
+
+
+# %%
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             hue='model_options_n_channels_len',
-             x='model_options_n_channels_0',
-             style='model_options_dilation_sizes_1',
-             data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
-ax.set_yscale('log')
-show_fig(fig, False)
+sns.boxplot(y='rmse',
+            x='model_options_dilation_sizes_1',
+            data=results_tcn,
+            )
 
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+ax.set_xticklabels(['no dilations', 'with dilations'])
+plt.tight_layout()
+plt.savefig('dilations_f16gvt.png')
+show_fig(fig, False)
 
 # %% (TCN) kernel size vs depth
 
 fig, ax = plt.subplots()
-sns.lineplot(y='vloss',
-             x='model_options_n_channels_len',
-             hue='model_options_ksize',
-             data=results_tcn,
-             legend='full',
-             estimator='min',
-             ci=None)
-ax.set_yscale('log')
+sns.boxplot(y='rmse',
+            x='model_options_n_channels_len',
+            hue='model_options_n_channels_0',
+            data=results_tcn,
+            )
+
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('number of residual blocks', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+ax.set_xticklabels([1, 2, 4, 8])
+plt.tight_layout()
+plt.savefig('nlayers_chen.png')
 show_fig(fig, False)
 
+#%%
+fig, ax = plt.subplots()
+sns.boxplot(y='rmse',
+            x='model_options_n_channels_len',
+            data=results_tcn,
+            )
+
+
+ax.tick_params(axis='both', which='major', labelsize=16)
+ax.set_xlabel('number of residual blocks', fontsize=16)
+ax.set_ylabel('RMSE', fontsize=16)
+ax.set_xticklabels([1, 2, 4, 8])
+plt.tight_layout()
+plt.savefig('nlayers_f16gvt.png')
+show_fig(fig, False)
 
 # %% (TCN) kernel size vs num channels
 
 fig, ax = plt.subplots()
 sns.lineplot(y='vloss',
              x='model_options_n_channels_0',
-             hue='model_options_ksize',
+             #hue='model_options_ksize',
              data=results_tcn,
              legend='full',
              estimator='min',
